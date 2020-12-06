7eb925f3c778d49607fd8f6b66cb328ad85f90c7
risulkarimx1 <risulkarimx1@gmail.com>
2020-12-06 21:35:38 +0100

    2 ways to create a task

---
+++ MultiThreading/MultiThreading/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.CompilerServices;
+
+namespace MultiThreading
+{
+    class Program
+    {
+        // ways to make tasks
+
+        public static void Write(Char c)
+        {
+            var i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(c);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Task.Factory.StartNew(() => Write('_')); // starts right away
+            Task t = new Task(()=> Write('-')); // creates a task
+            t.Start();// now start
+            Write(','); // running on main thread
+        }
+    }
+}
