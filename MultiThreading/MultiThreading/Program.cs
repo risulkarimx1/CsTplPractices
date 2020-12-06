34dc1e9d1d2c82adeff3c509be7240f4843d22a4
risulkarimx1 <risulkarimx1@gmail.com>
2020-12-06 22:15:17 +0100

    IsCancell registration with token

--- MultiThreading/MultiThreading/Program.cs
+++ MultiThreading/MultiThreading/Program.cs
@@ -20,21 +20,28 @@ namespace MultiThreading
             }
         }
 
+        static void IsCanceled(int? taskId)
+        {
+            Console.WriteLine($"task id {taskId} is cancelled");
+        }
+
         static void Main(string[] args)
         {
             var cts = new CancellationTokenSource();
             var token = cts.Token;
             var t = new Task(()=> NumberGenerator(token));
+            token.Register( () => IsCanceled(1)); // to get notified when task is cancelled
             t.Start();
 
             var cts2 = new CancellationTokenSource();
-            var t2 = new Task(() => NumberGenerator(cts2.Token));
+            var token2 = cts2.Token;
+            var t2 = new Task(() => NumberGenerator(token2));
             t2.Start();
+            token2.Register(() => IsCanceled(2));
 
             Console.ReadKey();
             cts.Cancel();
             cts2.Cancel();
-
 
             Console.WriteLine("Main program done");
             Console.ReadKey();
