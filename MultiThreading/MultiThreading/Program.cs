8bcf400352f9abb413db90ea73a1cef84c52f4f4
risulkarimx1 <risulkarimx1@gmail.com>
2020-12-06 23:18:26 +0100

    Catching exeption using aggregated exception

--- MultiThreading/MultiThreading/Program.cs
+++ MultiThreading/MultiThreading/Program.cs
@@ -7,27 +7,35 @@ namespace MultiThreading
 {
     class Program
     {
-
-        static void WaitingTask(int time)
-        {
-            Console.WriteLine($"at task id {Task.CurrentId}");
-            Thread.Sleep(time);
-            Console.WriteLine($"Done task id {Task.CurrentId}");
-        }
-
         static void Main(string[] args)
         {
-            var t1 = new Task(() => WaitingTask(1000));
-            var t2 = new Task(() => WaitingTask(3000));
+            var t1 = new Task(() =>
+            {
+                throw new InvalidOperationException("Invalid ops exception"){Source =  "T1"};
+            });
+
+            var t2 = new Task(() =>
+            {
+                throw new AccessViolationException("invalid access"){Source = "T2"};
+            });
+
             t1.Start();
             t2.Start();
 
-            Task.WaitAny(new Task[]{ t1, t2 }, 2000); // added a max time span
-            Console.WriteLine($"Task 1 status {t1.Status}");
-            Console.WriteLine($"Task 2 status {t2.Status}");
+            try
+            {
+                // Exception only gets propagated with wait all/any is called
+                Task.WaitAll(t1, t2);
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var ex in ae.InnerExceptions)
+                {
+                    Console.WriteLine($"Exception of type {ex.GetType()} from source: {ex.Source}");
+                }
+            }
 
-            Console.WriteLine("Done with tasks");
-
+            Console.WriteLine("Completed");
             Console.ReadKey();
         }
     }}
