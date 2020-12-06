a199b168ce8a6bf8f1699fa25deb971d7cb80a38
risulkarimx1 <risulkarimx1@gmail.com>
2020-12-06 22:58:54 +0100

    Checking status of task when wait any is called

--- MultiThreading/MultiThreading/Program.cs
+++ MultiThreading/MultiThreading/Program.cs
@@ -17,12 +17,14 @@ namespace MultiThreading
 
         static void Main(string[] args)
         {
-            var t1 = new Task(() => WaitingTask(2000));
+            var t1 = new Task(() => WaitingTask(1000));
             var t2 = new Task(() => WaitingTask(3000));
             t1.Start();
             t2.Start();
 
-            Task.WaitAny(new Task[]{ t1, t2 }, 1000); // added a max time span
+            Task.WaitAny(new Task[]{ t1, t2 }, 2000); // added a max time span
+            Console.WriteLine($"Task 1 status {t1.Status}");
+            Console.WriteLine($"Task 2 status {t2.Status}");
 
             Console.WriteLine("Done with tasks");
 
