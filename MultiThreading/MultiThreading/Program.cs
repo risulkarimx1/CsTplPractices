4b1c4fe8ee45386476c6bd73676b309c3ce5676b
risulkarimx1 <risulkarimx1@gmail.com>
2020-12-10 00:16:23 +0100

    Using mutex to lock the unsafe portion (non atomic code)

--- MultiThreading/MultiThreading/Program.cs
+++ MultiThreading/MultiThreading/Program.cs
@@ -18,46 +18,66 @@ namespace MultiThreading
         public void AddBalance(int amt)
         {
             _balance += amt;
+//            Interlocked.Add(ref _balance, amt);
         }
 
         public void DeductBalance(int amt)
         {
             _balance -= amt;
+//            Interlocked.Add(ref _balance, -amt);
         }
     }
 
     class Program
     {
-        private static SpinLock sl = new SpinLock(true);
-        public static void LockRecursion(int x)
-        {
-            bool lockTaken = false;
-            try
-            {
-                sl.Enter(ref lockTaken);
-            }
-            catch (LockRecursionException e)
-            {
-                Console.WriteLine($"exception is {e}");
-            }
-            finally
-            {
-                if (lockTaken)
-                {
-                    Console.WriteLine($"Lock was taken with the value {x}");
-                    LockRecursion(x-1);
-                    sl.Exit();
-                }
-                else
-                {
-                    Console.WriteLine($"failed to take a lock with the value {x}");
-                }
-            }
-        }
 
         private static void Main(string[] arg)
         {
-            LockRecursion(5);
+            BankAccount ba = new BankAccount();
+            var mutex = new Mutex();
+
+            var task = new List<Task>();
+
+            for (int j = 0; j < 10; j++)
+            {
+                task.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        var canlock = mutex.WaitOne();
+                        try
+                        {
+
+                            ba.AddBalance(10);
+                        }
+                        finally
+                        {
+                            if(canlock) mutex.ReleaseMutex();
+                        }
+                    }
+                }));
+
+                task.Add(Task.Factory.StartNew(() =>
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        var canlock = mutex.WaitOne();
+                        try
+                        {
+
+                            ba.DeductBalance(10);
+                        }
+                        finally
+                        {
+                            if (canlock) mutex.ReleaseMutex();
+                        }
+                    }
+                }));
+            }
+
+            Task.WaitAll(task.ToArray());
+
+            Console.WriteLine($"Final balance {ba.Balance}");
         }
     }
 }
