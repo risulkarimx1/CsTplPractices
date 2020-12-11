cf49130a49d0777c529c380461d412d169765980
risulkarimx1 <risulkarimx1@gmail.com>
2020-12-11 01:51:28 +0100

    Added BlockingCollection wrapper with producer consumer process with limit to 10

--- MultiThreading/MultiThreading/Program.cs
+++ MultiThreading/MultiThreading/Program.cs
@@ -1,34 +1,62 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace MultiThreading
 {
     class Program
     {
+        // while adding 11th element, it will block the process until there are placed
+        static BlockingCollection<int> messages = new BlockingCollection<int>(new ConcurrentBag<int>(), 10);
         
-
+        static CancellationTokenSource cts = new CancellationTokenSource();
+        private static Random rand = new Random();
         public static void Main(string[] args)
         {
-            ConcurrentBag<int> bag = new ConcurrentBag<int>();
-            var tasks = new List<Task>();
-            for (int i = 0; i < 10; i++)
-            {
-                var i1 = i;
-                tasks.Add(Task.Factory.StartNew(() =>
-                {
-                    bag.Add(i1);
-                    Console.WriteLine($"Task id : {Task.CurrentId} has added {i1}");
-                    int val;
-                    if (bag.TryPeek(out val))
-                    {
-                        Console.WriteLine($"Task id: {Task.CurrentId} has peeked {val}");
-                    }
-                }));
-            }
+            Task.Factory.StartNew(ProduceAndConsume, cts.Token);
+            Console.ReadKey();
+            cts.Cancel();
+        }
 
-            Task.WaitAll(tasks.ToArray());
+        private static void ProduceAndConsume()
+        {
+            var producer = Task.Factory.StartNew(RunProducer);
+            var consumer = Task.Factory.StartNew(Consumer);
+            try
+            {
+                Task.WaitAll(new[] {producer, consumer}, cts.Token);
+            }
+            catch (AggregateException aggregate)
+            {
+                aggregate.Handle(e => true);
+            }
+        }
+
+        private static void Consumer()
+        {
+            // as soon as some one adds something to the message, its going to give us this enumeration
+            // its gonna wait being blocked for the next element to come in
+            // GetConsuming means we consumed and got it in the variable item
+            foreach (var item in messages.GetConsumingEnumerable())
+            {
+                cts.Token.ThrowIfCancellationRequested();
+                Console.WriteLine($"removed {item}");
+                Thread.Sleep(rand.Next(1000));
+            }
+        }
+
+        private static void RunProducer()
+        {
+            while (true)
+            {
+                cts.Token.ThrowIfCancellationRequested();
+                int i = rand.Next(10);
+                messages.Add(i);
+                Console.WriteLine($"added {i}");
+                Thread.Sleep(rand.Next(100));
+            }
         }
     }
 }
