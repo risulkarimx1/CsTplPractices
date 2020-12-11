8d820617350f1600cdd0072bb1a1d304ce5df048
risulkarimx1 <risulkarimx1@gmail.com>
2020-12-11 01:16:37 +0100

    Concurrent bag

--- MultiThreading/MultiThreading/Program.cs
+++ MultiThreading/MultiThreading/Program.cs
@@ -1,41 +1,34 @@
 ﻿using System;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace MultiThreading
 {
     class Program
     {
-        static ConcurrentDictionary<string, string> capitals = new ConcurrentDictionary<string, string>();
-        public static void AddParis()
-        {
-            var success = capitals.TryAdd("France", "Paris");
-            var who = Task.CurrentId.HasValue ? $"Task {Task.CurrentId}" : "Main Thread";
-            Console.WriteLine($"{who} could add value with success {success}");
-        }
+        
 
         public static void Main(string[] args)
         {
-            capitals["Russia"] = "Leningrad";
-            capitals.AddOrUpdate("Russia", "Moscow", (key, oldValue) => $"{oldValue} -> Moscow" );
-            Console.WriteLine($"capital of russia is {capitals["Russia"]}");
-            var toRemove = "Russia";
-            string removed;
-            var couldRemove = capitals.TryRemove(toRemove, out removed);
-            if (couldRemove)
+            ConcurrentBag<int> bag = new ConcurrentBag<int>();
+            var tasks = new List<Task>();
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine($"removed capital value was {removed}");
+                var i1 = i;
+                tasks.Add(Task.Factory.StartNew(() =>
+                {
+                    bag.Add(i1);
+                    Console.WriteLine($"Task id : {Task.CurrentId} has added {i1}");
+                    int val;
+                    if (bag.TryPeek(out val))
+                    {
+                        Console.WriteLine($"Task id: {Task.CurrentId} has peeked {val}");
+                    }
+                }));
             }
-            else
-            {
-                Console.WriteLine($"cant remove the object {toRemove}");
-            }
 
-            capitals["Sweden"] = "Upsala";
-
-
-//            var capOfSweden = capitals.GetOrAdd("Sweden", s => "Stockholm");
-//            Console.WriteLine($"Capital of sweden is {capOfSweden}");
+            Task.WaitAll(tasks.ToArray());
         }
     }
 }
