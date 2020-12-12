04b18c4fb3dd2f7e714d8e61659774ea3c9cda6a
risulkarimx1 <risulkarimx1@gmail.com>
2020-12-12 01:44:18 +0100

    Task.ContinueWithAny example

--- MultiThreading/MultiThreading/Program.cs
+++ MultiThreading/MultiThreading/Program.cs
@@ -10,15 +10,10 @@ namespace MultiThreading
             Task<string> task1 = Task.Factory.StartNew(() => "Task 1");
             Task<string> task2 = Task.Factory.StartNew(() => "Task 2");
 
-            var task3 = Task.Factory.ContinueWhenAll(new[] {task1, task2}, tasks =>
+            var task3 = Task.Factory.ContinueWhenAny(new[] {task1, task2}, t =>
             {
                 Console.WriteLine($"Task completed");
-                foreach (var task in tasks)
-                {
-                    Console.WriteLine($"{task.Result}");
-                }
-
-                Console.WriteLine($"All task completed");
+                Console.WriteLine($"{t.Result}");
             });
 
             task3.Wait();
