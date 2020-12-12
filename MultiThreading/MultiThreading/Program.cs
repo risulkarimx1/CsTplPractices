9af1b8c7829c18b90ae0c56edbc47c685c8bd28b
risulkarimx1 <risulkarimx1@gmail.com>
2020-12-12 02:21:01 +0100

    Added success failure handle task passed by options

--- MultiThreading/MultiThreading/Program.cs
+++ MultiThreading/MultiThreading/Program.cs
@@ -15,7 +15,17 @@ namespace MultiThreading
                     Console.WriteLine($"child task started");
                     Thread.Sleep(5000);
                     Console.WriteLine("Child task finished");
+                    throw new Exception();
                 }, TaskCreationOptions.AttachedToParent);
+
+                Task successCompletionHandle = childTask.ContinueWith(
+                    t => { Console.WriteLine($"SUCCESS! Task {t.Id} status {t.Status}"); },
+                    TaskContinuationOptions.AttachedToParent | TaskContinuationOptions.OnlyOnRanToCompletion);
+
+                Task failureCompletionHandle = childTask.ContinueWith(
+                    t => { Console.WriteLine($"FAIULURE! Task {t.Id} status {t.Status}"); },
+                    TaskContinuationOptions.AttachedToParent | TaskContinuationOptions.NotOnRanToCompletion);
+
                 childTask.Start();
             });
 
